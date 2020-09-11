a1114701fdb78f7a52dfdac030a42e16ed92bc03
Natalie <nataliech0409@gmail.com>
2020-09-11 21:50:11 +0300

    Added all files

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const  selectAllBtn = document.getElementById('select-btn');
+const inputElement = document.getElementById('input');
+
+selectAllBtn.addEventListener('click', () => {console.log('Hello!')});
+inputElement.addEventListener('keydown', () => {console.log('Yoyo!')});
+
